c5cb7758d640545d33efa2db493a7934014ebafc
loganitis899 <league@WTS-IM-6.attlocal.net>
2018-09-03 16:59:54 -0700

    We played a game about robots flying in space and if we flew the robot of R2D2, R2D2 would make a beeping noise.

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -2,18 +2,21 @@ package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score=0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+JOptionPane.showInputDialog("There was a green house. Inside the green house there was a white house. Inside the white house there was a red house. Inside the red house there were lots of babies. What is it?");
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+ if () {
+	
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
