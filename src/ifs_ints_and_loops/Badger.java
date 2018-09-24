48324370dcbae69e4e745895f5e8725eacda2967
nzsoccer <league@WTS-iMac-11.attlocal.net>
2018-09-24 16:56:22 -0700

    I finished the rocket and then I made the computer print out the lyrics of a song. Cool!

---
+++ src/ifs_ints_and_loops/Badger.java
@@ -0,0 +1,13 @@
+package ifs_ints_and_loops;
+
+public class Badger {
+	public static void main(String[] args) {
+
+		String b = "badger";
+		String m = "mushroom";
+		for (int i = 0; i < 1; i++) {
+			System.out.println("badger,badger, badger, badger, badger, badger, badger, badger mushroom, mushroom");
+		}
+	}
+
+}
